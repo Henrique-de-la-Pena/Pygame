41f2bc51060caa4772fcfd73b124dd1c01ed7750
Rafael <rafaelba10@al.insper.edu.br>
2025-05-26 09:56:59 -0300

    Config inicial

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import path
+
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'fnt')
+
+WIDTH = 600
+HEIGHT = 300
+FPS = 60
+
+#CAT_WIDTH = 
+#CAT_HEIGHT =
+#CAR_WIDTH =
+#CAR_HEIGHT = 
+#COIN_WIDTH = 
+#COIN_HEIGHT = 
+
+INIT = 0
+GAME = 1
+QUIT = 2
