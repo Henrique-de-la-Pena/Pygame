c06ad8205f2c79a01251e745c23e3c8718d497ed
Rafael <rafaelba10@al.insper.edu.br>
2025-05-26 09:39:46 -0300

    Código jogo

---
+++ jogo.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+from config import *
+from init_screen import init_screen
+from game_screen import game_screen
+
+pygame.init()
+pygame.mixer.init()
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = game_screen(window)
+    else:
+        state = QUIT
+
+pygame.quit()
