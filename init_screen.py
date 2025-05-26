5a35f1c889bfe1ec3dedcda1ea27680f32a7897f
gustavo <gustavovp3@al.insper.edu.br>
2025-05-26 13:35:41 -0300

    Tela inicial

---
+++ init_screen.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from os import path
+from config import *
+
+def init_screen(screen):
+    clock = pygame.time.Clock()
+
+    #background = pygame.image.load(path.join(IMG_DIR, '')).convert()
+    #background_rect = background.get_rect()
+
+    running = True
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+            
+            if event.type == pygame.KEYUP:
+                state = GAME
+                running = False
+
+        screen.fill((0, 0, 0))
+        #screen.blit(background, background_rect)
+
+        pygame.display.flip()
+
+    return state
