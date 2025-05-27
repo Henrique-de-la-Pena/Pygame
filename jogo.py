03aa4fc5c35e22e6dbb295c4e7b47975ab047c6e
Rafael <rafaelba10@al.insper.edu.br>
2025-05-27 17:46:13 -0300

    Score e lives em jogo

--- jogo.py
+++ jogo.py
@@ -3,19 +3,27 @@ import random
 from config import *
 from init_screen import init_screen
 from game_screen import game_screen
+from game_over_screen import game_over_screen
 
 pygame.init()
 pygame.mixer.init()
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('')
+pygame.display.set_caption('Caça Rato')
+score = 0
+lives = 7
 
 state = INIT
 while state != QUIT:
     if state == INIT:
         state = init_screen(window)
     elif state == GAME:
-        state = game_screen(window)
+        gameplayed = game_screen(window, score, lives)
+        state = gameplayed[0]
+        score = gameplayed[1]
+        lives = gameplayed[2]
+    elif state == GAME_OVER:
+        state = game_over_screen(window)
     else:
         state = QUIT
 
